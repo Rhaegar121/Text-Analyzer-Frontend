bcb2da05fb0b7b4f08e47d8b1450860446527d17
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-09-19 15:54:14 +0630

    display analysisResults in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import FileUpload from './FileUpload'
+import TextAnalysis from './TextAnalysis'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [analysisResults, setAnalysisResults] = useState(null);
+
+  const handleFileUpload = async (file) => {
+    const formData = new FormData();
+    formData.append('file', file);
+    const response = await fetch('http://127.0.0.1:3000/api/text_analysis/analyze', {
+      method: 'POST',
+      body: formData,
+    });
+    const data = await response.json();
+    setAnalysisResults(data);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Text Analyzer</h1>
+      <FileUpload onFileUpload={handleFileUpload} />
+      {analysisResults && <TextAnalysis analysisResults={analysisResults} />}
     </>
   )
 }
