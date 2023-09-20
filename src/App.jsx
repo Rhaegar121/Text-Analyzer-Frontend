ff43383a52cb7403992add117233f930e7428e0f
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-09-20 12:40:52 +0630

    add loading state in app component

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,41 @@ import './App.css'
 
 function App() {
   const [analysisResults, setAnalysisResults] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleFileUpload = async (file) => {
+    setIsLoading(true);
     const formData = new FormData();
     formData.append('file', file);
-    const response = await fetch('http://127.0.0.1:3000/api/text_analysis/analyze', {
-      method: 'POST',
-      body: formData,
-    });
-    const data = await response.json();
-    setAnalysisResults(data);
+
+    try {
+      const response = await fetch('http://127.0.0.1:3000/api/text_analysis/analyze', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setAnalysisResults(data);
+      } else {
+        console.error('Failed to fetch data');
+      }
+    } catch (error) {
+      console.error('An error occurred:', error);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
     <div className="App">
       <h1>Text Analyzer</h1>
       <FileUpload onFileUpload={handleFileUpload} />
-      {analysisResults && <TextAnalysis analysisResults={analysisResults} />}
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : analysisResults ? (
+        <TextAnalysis analysisResults={analysisResults} />
+      ) : null}
     </div>
   )
 }
